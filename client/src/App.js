0135a025a8d0bec4880ecd5b52a79af04df1eb13
Sabrina Mueller <sabrina@famefox.de>
2021-06-06 18:56:54 +0200

    started to build basic structure of the app by creating folders and implementing react-router-dom into app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,69 @@
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
 
 function App() {
-  const [serverMessage, setServerMessage] = useState('');
-
-  useEffect(() => {
+  /*   useEffect(() => {
     fetch('http://localhost:4000/')
       .then((res) => res.json())
       .then((response) => setServerMessage(response));
-  });
-  return <div className='App'>{serverMessage}</div>;
+  }); */
+
+  function Home() {
+    return <></>;
+  }
+
+  function AnimalSearch() {
+    return <></>;
+  }
+
+  function AnimalForm() {
+    return <></>;
+  }
+
+  function Main() {
+    return <></>;
+  }
+
+  function Favorites() {
+    return <></>;
+  }
+
+  function Contact() {
+    return <></>;
+  }
+
+  return (
+    <main className='App'>
+      <Navigation />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/search'>
+          <AnimalSearch />
+        </Route>
+        <Route path='/add-animal'>
+          <AnimalForm />
+        </Route>
+        <Route path='/main'>
+          <Main />
+        </Route>
+        <Route path='/favorites'>
+          <Favorites />
+        </Route>
+        <Route path='/contact'>
+          <Contact />
+        </Route>
+        {/*         <Route path='/about-organization'>
+          <OrgnaizationInfo />
+        </Route>
+        <Route path='/about-breed'>
+          <BreedInfo />
+        </Route> */}
+      </Switch>
+    </main>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <Router>
+      <GlobalStyle />
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
